e9e4d88e8d75211cf491060aa5506dab5bb56d30
antenen21 <info.antenen@gmail.com>
2023-07-30 13:53:18 +0200

    adding the TEST and REAL JS scripts for carousel in script.js

--- static/js/script.js
+++ static/js/script.js
@@ -8,3 +8,86 @@
       function closeNav() {
       document.getElementById("mySidenav").style.width = "0";
       }
+
+
+
+
+
+
+
+/*----------------------------------------------
+|           TEST CAROUSEL         |
+-----------------------------------------------*/
+      
+      $(document).ready(function() {
+   
+         const shopSlider = $('#shopSlider');
+     
+         shopSlider.owlCarousel({
+             loop: true,
+             dots: false,
+             margin: 2,
+             smartSpeed: 500,
+             responsive: {
+                 // breakpoint from 0 up
+                 0: {
+                     items: 1,
+                 },
+                 // breakpoint from 3 up
+                 1254: {
+                     items: 3,
+                 }
+             }
+     
+         });
+     
+         $('#shopSliderLeft').click(function() {
+             console.log('Left');
+             shopSlider.trigger('prev.owl.carousel');
+         });
+     
+         $('#shopSliderRight').click(function() {
+             console.log('Right');
+             shopSlider.trigger('next.owl.carousel');
+         });
+     
+     });
+
+
+/*----------------------------------------------
+|           INDEX PUMP SLIDER                   |
+-----------------------------------------------*/
+      
+$(document).ready(function() {
+   
+      const shopSlider = $('#main_slider');
+  
+      shopSlider.owlCarousel({
+          loop: true,
+          dots: false,
+          margin: 2,
+          smartSpeed: 500,
+          responsive: {
+              // breakpoint from 0 up
+              0: {
+                  items: 1,
+              },
+              // breakpoint from 3 up
+              1254: {
+                  items: 3,
+              }
+          }
+  
+      });
+  
+      $('#pumpLeft').click(function() {
+          console.log('Left');
+          shopSlider.trigger('prev.owl.carousel');
+      });
+  
+      $('#pumpRight').click(function() {
+          console.log('Right');
+          shopSlider.trigger('next.owl.carousel');
+      });
+  
+  });
